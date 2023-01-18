b5942551f07522d0869974d808945d39a2ee157a
dps <dobro.polya@yandex.ru>
2023-01-18 08:59:11 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,41 +4,20 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int o=1;// для диалогового окна
-        double m = 0;// кол-во денег на счету у пользователя
-        int bns = 0;//сумма пополнения
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); //для считывания данных с клавиатуры
-        System.out.println("На вашем счету:" +m);
-        while (o==1)
-        {   System.out.println("Введите сумму пополнения:");
-            String c = reader.readLine();
-            double summa = Double.parseDouble(c);
-            if (summa>1000)
-            {
-                bns=(int)(m+summa)/100;
-                m=(m+summa+bns);
-            }
-            else
-            {
-                   m=m+summa;
-            }
-           // m = (int) costs / cnt;
-            System.out.println("Количество бонусов " + bns);
-            System.out.println("Количество денег на вашем счету " + m);
-            System.out.println("Хотите пополнить еще? Если да, то введите цифру 1, если нет - то любую другую цифру");
-            String c1 = reader.readLine();
-            o=Integer.parseInt(c1);
-
-            if (o!=1)
-            {
-                System.out.println("Хотите завершить операцию? Нажмите q");
-                String q = reader.readLine();
-                if (q=="q")
-                {
-                    System.exit(0);
-                }
-            }
-
+        double money = 0;// кол-во денег на счету у пользователя
+        int bonus = 0;//сумма пополнения
+        System.out.println("На вашем счету:" + money);
+        double replenishmentAmount = 3540.40;
+        System.out.println("Сумма пополнения:" + replenishmentAmount);
+        if (replenishmentAmount > 1000) {
+            bonus = (int) (money + replenishmentAmount) / 100;
+            money = (money + replenishmentAmount + bonus);
+        } else {
+            money = money + replenishmentAmount;
         }
+        System.out.println("Количество бонусов " + bonus);
+        System.out.println("Количество денег на вашем счету " + money);
+
     }
+}
 }
